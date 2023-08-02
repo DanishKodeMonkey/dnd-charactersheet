f6846533614a3868864bfeee2f6ded9a2087f355
danishkodemonkey <humberjini@hotmail.com>
2023-08-02 16:38:15 +0200

    Cleaned up deprecated files

--- src/Pages/Inventory.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Inventory = () => {
-    return (
-        <h1>Inventory page</h1>
-    )
-}
-
-export default Inventory;
--- src/Pages/Skills.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Skills = () => {
-    return (
-        <h1>Skills page</h1>
-    )
-}
-
-export default Skills;
