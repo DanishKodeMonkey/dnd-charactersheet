7e41b5b5adc1a36b818b52f4a1c32e008aaac0f4
danishkodemonkey <humberjini@hotmail.com>
2023-08-20 10:05:05 +0200

    Implemented dataset objects to Skillslist, now properly forming objects holding the list of skills and their state.

--- src/Components/SkillsComponents/SkillRow.js
+++ src/Components/SkillsComponents/SkillRow.js
@@ -1,52 +1,89 @@
-import React from "react";
-import { Form, Col, Row, Container} from "react-bootstrap";
-import './Skills.css'
+import React from "react"
+import { Form, Col, Row, Container } from "react-bootstrap"
+import "./Skills.css"
 const SkillRow = (props) => {
-  const { skillName, keyAbility, skillModifier, abilityModifier, ranks, miscModifier, handleInputChange } = props;
+  const {
+    classSkill,
+    skillName,
+    keyAbility,
+    skillModifier,
+    abilityModifier,
+    ranks,
+    miscModifier,
+    handleInputChange,
+  } = props
 
   return (
     <Container>
-    <Row>
-      <Form.Group as={Col} sm={1} style={{width:"auto"}}>
-        <Form.Check type="checkbox" aria-label="classskill"/>
-      </Form.Group>
-      <Form.Group as={Col} sm={4} className="no-gutter">
-        <Form.Label>{skillName}</Form.Label>
-      </Form.Group>
-      <Form.Group as={Col} sm={3} className="no-gutter">
-        {keyAbility}
-      </Form.Group>
-      <Form.Group as={Col} sm={1}  className="no-gutter">
-        <Form.Control
-          type="number"
-          value={skillModifier}
-          onChange={(e) => handleInputChange("skillModifier", e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group as={Col} sm={1} className="no-gutter">
-        <Form.Control
-          type="number"
-          value={abilityModifier}
-          onChange={(e) => handleInputChange("abilityModifier", e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group as={Col} sm={1} className="no-gutter">
-        <Form.Control
-          type="number"
-          value={ranks}
-          onChange={(e) => handleInputChange("ranks", e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group as={Col} sm={1} className="no-gutter">
-        <Form.Control
-          type="number"
-          value={miscModifier}
-          onChange={(e) => handleInputChange("miscModifier", e.target.value)}
-        />
-      </Form.Group>
+      <Row>
+        <Form.Group
+          as={Col}
+          sm={1}
+          style={{ width: "auto" }}>
+          <Form.Check
+            type="checkbox"
+            aria-label="classskill"
+            checked={classSkill}
+            onChange={(e) => handleInputChange("classSkill", e.target.checked)}
+          />
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          sm={4}
+          className="no-gutter">
+          <Form.Label>{skillName}</Form.Label>
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          sm={3}
+          className="no-gutter">
+          {keyAbility}
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          sm={1}
+          className="no-gutter">
+          <Form.Control
+            type="number"
+            value={skillModifier}
+            onChange={(e) => handleInputChange("skillModifier", e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          sm={1}
+          className="no-gutter">
+          <Form.Control
+            type="number"
+            value={abilityModifier}
+            onChange={(e) =>
+              handleInputChange("abilityModifier", e.target.value)
+            }
+          />
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          sm={1}
+          className="no-gutter">
+          <Form.Control
+            type="number"
+            value={ranks}
+            onChange={(e) => handleInputChange("ranks", e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          sm={1}
+          className="no-gutter">
+          <Form.Control
+            type="number"
+            value={miscModifier}
+            onChange={(e) => handleInputChange("miscModifier", e.target.value)}
+          />
+        </Form.Group>
       </Row>
     </Container>
-  );
-};
+  )
+}
 
-export default SkillRow;
+export default SkillRow
--- src/Components/SkillsComponents/Skills.js
+++ src/Components/SkillsComponents/Skills.js
@@ -4,10 +4,11 @@ import SkillRow from "./SkillRow"
 import SkillRowLabel from "./SkillRowLabel"
 import SkillRowLabelMobile from "./SkillRowLabelMobile"
 
-const SkillList = () => {
+const SkillList = ({ onDataChange }) => {
   const [data, setData] = useState([
     // Initial data for each skill row
     {
+      classSkill: false,
       skillName: "appraise",
       keyAbility: "INT",
       skillModifier: 0,
@@ -16,6 +17,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "balance",
       keyAbility: "DEX*",
       skillModifier: 0,
@@ -24,6 +26,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "bluff",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -32,6 +35,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "climb",
       keyAbility: "STR*",
       skillModifier: 0,
@@ -40,6 +44,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "concentration",
       keyAbility: "CON",
       skillModifier: 0,
@@ -48,6 +53,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "craft1",
       keyAbility: "INT",
       skillModifier: 0,
@@ -56,6 +62,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "craft2",
       keyAbility: "INT",
       skillModifier: 0,
@@ -64,6 +71,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "craft3",
       keyAbility: "INT",
       skillModifier: 0,
@@ -72,6 +80,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "decipherscript",
       keyAbility: "INT",
       skillModifier: 0,
@@ -80,6 +89,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "diplomacy",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -88,6 +98,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "disabledevice",
       keyAbility: "INT",
       skillModifier: 0,
@@ -96,6 +107,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "disguise",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -104,6 +116,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "escapeartist",
       keyAbility: "DEX*",
       skillModifier: 0,
@@ -112,6 +125,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "forgery",
       keyAbility: "INT",
       skillModifier: 0,
@@ -120,6 +134,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "gatherinformation",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -128,6 +143,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "handleanimal",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -136,6 +152,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "heal",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -144,6 +161,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "hide",
       keyAbility: "DEX*",
       skillModifier: 0,
@@ -152,6 +170,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "intimidate",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -160,6 +179,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "jump",
       keyAbility: "STR*",
       skillModifier: 0,
@@ -168,6 +188,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "knowledge1",
       keyAbility: "INT",
       skillModifier: 0,
@@ -176,6 +197,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "knowledge2",
       keyAbility: "INT",
       skillModifier: 0,
@@ -184,6 +206,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "knowledge3",
       keyAbility: "INT",
       skillModifier: 0,
@@ -192,6 +215,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "knowledge4",
       keyAbility: "INT",
       skillModifier: 0,
@@ -200,6 +224,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "knowledge5",
       keyAbility: "INT",
       skillModifier: 0,
@@ -208,6 +233,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "listen",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -216,6 +242,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "movesilently",
       keyAbility: "DEX*",
       skillModifier: 0,
@@ -224,6 +251,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "openlock",
       keyAbility: "DEX",
       skillModifier: 0,
@@ -232,6 +260,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "perform1",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -240,6 +269,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "perform2",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -248,6 +278,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "perform3",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -256,6 +287,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "profession1",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -264,6 +296,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "profession2",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -272,6 +305,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "ride",
       keyAbility: "DEX",
       skillModifier: 0,
@@ -280,6 +314,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "search",
       keyAbility: "INT",
       skillModifier: 0,
@@ -288,6 +323,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "sensemotive",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -296,6 +332,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "sleightofhand",
       keyAbility: "DEX*",
       skillModifier: 0,
@@ -304,6 +341,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "spellcraft",
       keyAbility: "INT",
       skillModifier: 0,
@@ -312,6 +350,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "spot",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -320,6 +359,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "survival",
       keyAbility: "WIS",
       skillModifier: 0,
@@ -328,6 +368,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "sswim",
       keyAbility: "STR*",
       skillModifier: 0,
@@ -336,6 +377,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "tumble",
       keyAbility: "DEX*",
       skillModifier: 0,
@@ -344,6 +386,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "usemagicdevice",
       keyAbility: "CHA",
       skillModifier: 0,
@@ -352,6 +395,7 @@ const SkillList = () => {
       miscModifier: 0,
     },
     {
+      classSkill: false,
       skillName: "userope",
       keyAbility: "DEX",
       skillModifier: 0,
@@ -361,37 +405,48 @@ const SkillList = () => {
     },
   ])
 
+  //Funciton handling input change for a skill
   const handleInputChange = (index, field, value) => {
     const updatedData = [...data]
     updatedData[index][field] = value
     setData(updatedData)
+    //Call the parents callback function with the update
+    onDataChange(updatedData)
   }
 
   return (
     <Container>
       <Form>
-      <Row>
+        <Row>
+          <SkillRowLabel />
 
-        <SkillRowLabel />
-
-        {data.map((skill, index) => (
-          <React.Fragment key={index}>
-          <Col xs="4" className="sm-d-none"><SkillRowLabelMobile /></Col>
-          <Col xs="8" sm="12" className="pt-2"><SkillRow
-            key={index}
-            skillName={skill.skillName}
-            keyAbility={skill.keyAbility}
-            skillModifier={skill.skillModifier}
-            abilityModifier={skill.abilityModifier}
-            ranks={skill.ranks}
-            miscModifier={skill.miscModifier}
-            handleInputChange={(field, value) =>
-              handleInputChange(index, field, value)
-            }
-          /></Col>
-          </React.Fragment>
-        ))}
-        
+          {data.map((skill, index) => (
+            <React.Fragment key={index}>
+              <Col
+                xs="4"
+                className="sm-d-none">
+                <SkillRowLabelMobile />
+              </Col>
+              <Col
+                xs="8"
+                sm="12"
+                className="pt-2">
+                <SkillRow
+                  key={index}
+                  classSkill={skill.classSkill}
+                  skillName={skill.skillName}
+                  keyAbility={skill.keyAbility}
+                  skillModifier={skill.skillModifier}
+                  abilityModifier={skill.abilityModifier}
+                  ranks={skill.ranks}
+                  miscModifier={skill.miscModifier}
+                  handleInputChange={(field, value) =>
+                    handleInputChange(index, field, value)
+                  }
+                />
+              </Col>
+            </React.Fragment>
+          ))}
         </Row>
       </Form>
     </Container>
